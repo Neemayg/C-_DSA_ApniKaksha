69cb600ae7a21e09be3bf63821f2ea27b6f2e868
neemay <neemay.gupta2007@gmail.com>
2026-01-02 14:20:19 +0530

    final apna college segregation

--- basics(lecture-1)/User_input.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-  double price;
-  cout << "Enter your price : ";
-  cin >> price;
-  cout << "Your Entered price is : " << price << endl;
-  return 0;
-}
